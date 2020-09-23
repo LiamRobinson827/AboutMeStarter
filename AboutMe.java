dfa97ca7f314b45ee5a451aeda421fadbe779f54
LiamRobinson827 <57147116+LiamRobinson827@users.noreply.github.com>
2020-09-23 08:41:48 -0400

    added the TODO'S

--- AboutMe.java
+++ AboutMe.java
@@ -1,6 +1,6 @@
 /*
- * Programmer:
- * Date:
+ * Programmer:Liam Robinson
+ * Date: Sep,23
  * Purpose:
  */
 
@@ -8,6 +8,9 @@ public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
         // TODO: Write the code the declares the variables
+    	String myName = "Liam";
+    	int myAge = 17;
+    	double myHeightInMetres = 187.96; 
 
         // Print the values
         System.out.println("Hi, my name is " + myName);
@@ -16,9 +19,14 @@ public class AboutMe {
 
         // Change the variables
         // TODO: Write the code that changes the variables
+         myAge= 22;
+         myHeightInMetres = 195.58;
 
         // Print the new values
         System.out.println("Some time passes...");
+        System.out.println("Hi, my name is" + myName);
+        System.out.println("I am " + myAge + " years old.'");
+        System.out.println("My height is " + myHeightInMetres + " m.");
         // TODO: Write the last two print statements
     }
 }
