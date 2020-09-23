823a43a5279eefca179a955a0a460c2ee0327382
LiamRobinson827 <57147116+LiamRobinson827@users.noreply.github.com>
2020-09-23 08:44:13 -0400

    Update AboutMe.java

--- AboutMe.java
+++ AboutMe.java
@@ -7,7 +7,6 @@
 public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
-        // TODO: Write the code the declares the variables
     	String myName = "Liam";
     	int myAge = 17;
     	double myHeightInMetres = 187.96; 
@@ -18,15 +17,14 @@ public class AboutMe {
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
-        // TODO: Write the code that changes the variables
          myAge= 22;
          myHeightInMetres = 195.58;
 
         // Print the new values
         System.out.println("Some time passes...");
         System.out.println("Hi, my name is" + myName);
-        System.out.println("I am " + myAge + " years old.'");
-        System.out.println("My height is " + myHeightInMetres + " m.");
-        // TODO: Write the last two print statements
+        System.out.println("I am now " + myAge + " years old.'");
+        System.out.println("I have grown and am " + myHeightInMetres + " m tall.");
+        
     }
 }
